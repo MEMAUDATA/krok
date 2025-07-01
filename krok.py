9fe4def8e861305f79c61d5bf8087d8bb92615b0
MEMAUDATA <vannson.nicolas@gmail.com>
2025-07-01 18:27:39 +0200

    Krok without logging for now

--- krok.py
+++ krok.py
@@ -1,42 +1,31 @@
-# https://epieos.com/pricing
+# Krok annual incommes' analysis
+# NV, Toulouse,07/2025
 
 import streamlit as st
+import os
 import pandas as pd
+import numpy as np
 
+from modules.synthesis import synthesis
 
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
-with open('krok.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
-)
-
-# Hearing data title
 st.html("<h1 style='text-align:center;color:#22573b;'>Krok</h1>")
 #color:#2B176C
-name, authentication_status, username = authenticator.login('main')
 
-if authentication_status:
-
-    if username == 'krok': # credentials used for demo
-        # Title section written with html
-        st.html("<p style='text-align:center;color:#22573b;'> Analyse économique annuelle et potentiellement global</p>")
-        st.html("""<div style="height: 10px;"></div>""")    
-        # upload section written with html
-        xmlfiles = st.file_uploader("Upload file(s) in .csv", accept_multiple_files=True,type=['.csv'])
-        st.html("""<div style="height: 10px;"></div>""") 
-        
-    authenticator.logout('Logout','main')
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
+ #Title section written with html
+st.html("<p style='text-align:center;color:#22573b;'> Analyse économique à plusieurs niveaux afin d'observer l'évolution du CA.<br> Il faut nommer chaque fichier en fonction de l'année (ex: 2023.xls) </p>")
+st.html("""<div style="height: 10px;"></div>""")    
+# upload section written with html
+csvfiles = st.file_uploader("Upload file(s) in .csv,.xls,.xlsx", accept_multiple_files=True,type=['.csv','.xls','xlsx'])
+st.html("""<div style="height: 10px;"></div>""") 
 
 
+#  Button to trigger processing
+if st.button("Process", key="process_button"):        
 
-
-
+    for file_name in csvfiles:
+        file = pd.read_excel(file_name,sheet_name=None)
+        income,df1 = synthesis(file)
+        #print((income))
+        file_stem, _ = os.path.splitext(file_name.name)
+        st.subheader(f"Année {file_stem}")
+        st.table(income)
---
+++ modules/synthesis.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+def synthesis(data):
+
+    """
+    Import raw data from xls/csv/xlsx into a dataframe.
+    NV, Toulouse, 07/2025
+
+    """
+
+    all_date = []
+    all_total = []
+    df = pd.DataFrame([])
+
+    for col_index, (col_name,col_data) in enumerate(data.items()):
+
+        if col_index > 0:
+            date_col = col_data.iloc[:,0]
+            all_date.append(date_col)
+            all_total.append(col_data['TOTAL']) 
+
+    combined_date  = pd.concat(all_date,axis=0,sort=False).to_frame(name='date')  
+    combined_total = pd.concat(all_total,axis=0,sort=False).to_frame(name='Total')
+    df = pd.concat([combined_date,combined_total],axis=1)
+    
+   
+
+    df['date'] = pd.to_datetime(df['date'], errors='coerce', format='%d/%m/%Y')  # Specify your date format!
+    df = df.dropna(subset=['date'])  # Drop rows with invalid dates
+    df = df.drop_duplicates(subset=['date'])  # Drop duplicate dates
+    df = df.replace(0, np.nan) #Replace 0 with NaN
+    df = df.dropna() #Drop NaN
+    df = df.reset_index(drop=True)  # Reset the index
+
+    df['Day of Week'] = df['date'].dt.day_name()  # Extract day of the week
+
+
+    income_per_day = df.groupby('Day of Week')['Total'].agg(['mean', 'std','sum'])
+    return income_per_day,df
